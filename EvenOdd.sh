f23f15d2f993838c5dcc7b9b347116a80ef47d19
sibasundar <sibasundar@email.com>
2023-06-11 05:17:55 +0000

    check modified currect file

--- EvenOdd.sh
+++ EvenOdd.sh
@@ -1,1 +1,10 @@
-its my even od file.
+#its my EvenOdd file i want to check
+#!bin/bash
+NUM=$1
+
+if [`expr $NUM % 2 `-eq 0 ]
+then 
+ echo num is even.
+else
+ echo num is odd.
+fi
--- readme.txt
+++ readme.txt
@@ -1,1 +1,2 @@
 hey please ream me its my new file.
+its my 2nd execution.
